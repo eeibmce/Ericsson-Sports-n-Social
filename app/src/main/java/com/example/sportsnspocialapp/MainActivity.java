1638bffd958384c89f709df85389ff321ea07a88
Rachel Reid <rachelreid0103@gmail.com>
2022-08-10 10:25:03 +0100

    Added chat activity and main page

--- app/src/main/java/com/example/sportsnspocialapp/MainActivity.java
+++ app/src/main/java/com/example/sportsnspocialapp/MainActivity.java
@@ -2,13 +2,62 @@ package com.example.sportsnspocialapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton button;
+    private ImageButton button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("We are in onCreate in Main Activity");
+        button = (ImageButton) findViewById(R.id.imageButton2);
+        TextView scrollingTool = findViewById(R.id.editTextTextMultiLine5);
+        scrollingTool.setMovementMethod(new ScrollingMovementMethod());
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openChat_page();
+            }
+        });
+        button2 = (ImageButton) findViewById(R.id.imageButton3);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCalendar();
+            }
+        });
     }
+
+    public void openChat_page() {
+        Intent intent = new Intent(this, Chat_page.class);
+        startActivity(intent);
+        setContentView(R.layout.activity_chat_page);
+    }
+
+    public void openCalendar() {
+        Intent intent = new Intent(this, Calendar3.class);
+        startActivity(intent);
+        //setContentView(R.layout.activity_calendar);
+    }
+
+    public void browser(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://sites.google.com/view/egsathlone/home"));
+        startActivity(browserIntent);
+
+    }
+
 }
