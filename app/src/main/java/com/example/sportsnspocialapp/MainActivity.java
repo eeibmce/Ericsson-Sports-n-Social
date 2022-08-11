7bc590667866bef0f7324b716b391883d3696e1d
gracemcglynn <mcglynng05@gmail.com>
2022-08-11 15:52:32 +0100

    splashscreen

--- app/src/main/java/com/example/sportsnspocialapp/MainActivity.java
+++ app/src/main/java/com/example/sportsnspocialapp/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.sportsnspocialapp;
 
+import android.os.Bundle;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+//    //Variables
+//    Animation topAnim, bottomAnim;
+//    ImageView imageView3;
+//    TextView textView, textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+//        //Animations
+//        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+//        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+//
+//        //Hooks
+//        imageView3 = findViewById(R.id.imageView3);
+//        textView = findViewById(R.id.textView);
+//        textView2 = findViewById(R.id.textView2);
+//
+//        imageView3.setAnimation(topAnim);
+//        textView.setAnimation(bottomAnim);
+//        textView2.setAnimation(bottomAnim);
+
     }
 }
