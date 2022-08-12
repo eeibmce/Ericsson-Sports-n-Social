cd6fba4211a50bdfa8d0ffb342f8e9502354c737
ConallMandal <conallpmandal@gmail.com>
2022-08-12 10:27:19 +0100

    main push home page

--- app/src/main/java/com/example/sportsnspocialapp/MainActivity.java
+++ app/src/main/java/com/example/sportsnspocialapp/MainActivity.java
@@ -2,13 +2,97 @@ package com.example.sportsnspocialapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+//import androidx.navigation.fragment.findNavController;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    //ImageButton btnAdd = (ImageButton)findViewById(R.id.imageButton2);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_home);
+
+//        Button contact = (Button) findViewById(R.id.button);
+//        String linkText = "<a href='https://www.ericsson.com/en/contact'>Contact Us</a>";
+//        contact.setText(Html.fromHtml(linkText));
+//        contact.setMovementMethod(LinkMovementMethod.getInstance());
+//
+
+        ImageButton btnAddPool = (ImageButton)findViewById(R.id.imageButtonPool);
+
+        btnAddPool.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                openPoolActivity();
+            }
+        });
+
+        ImageButton btnAddFootball = (ImageButton)findViewById(R.id.imageButtonFootball);
+
+        btnAddFootball.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                openFootballActivity();
+            }
+        });
+
+        ImageButton btnAddGolf = (ImageButton)findViewById(R.id.imageButton2);
+
+        btnAddGolf.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                openGolfActivity();
+            }
+        });
+
+        ImageButton btnAddBasketball = (ImageButton)findViewById(R.id.imageButtonBasketball);
+
+        btnAddBasketball.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                openBasketballActivity();
+            }
+        });
     }
+
+    public void openPoolActivity(){
+        Intent intent = new Intent(this, PoolActivity.class);
+        startActivity(intent);
+    }
+    public void openGolfActivity(){
+        Intent intent = new Intent(this, GolfActivity.class);
+        startActivity(intent);
+    }
+
+    public void openFootballActivity(){
+        Intent intent = new Intent(this, FootballActivity.class);
+        startActivity(intent);
+    }
+
+    public void openBasketballActivity(){
+        Intent intent = new Intent(this, BasketballActivity.class);
+        startActivity(intent);
+    }
+
+    public void browser(View view){
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ericsson.com/en/contact"));
+            startActivity(browserIntent);
+    }
+
 }
+
