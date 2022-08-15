01e8bc5be465cf638964790dc0ea84aabbeaf330
gracemcglynn <mcglynng05@gmail.com>
2022-08-15 16:20:57 +0100

    splash made

---
+++ app/src/main/java/com/example/sportsnspocialapp/SplashActivity.java
@@ -0,0 +1,66 @@
+package com.example.sportsnspocialapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+//    //Variables
+//    Animation topAnim, bottomAnim;
+//    ImageView imageView3;
+//    TextView textView, textView2;
+
+    private static int SPLASH_SCREEN_TIMEOUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+//        //Animations
+//        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+//        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+//
+//        //Hooks
+//        imageView3 = findViewById(R.id.imageView3);
+//        textView = findViewById(R.id.textView);
+//        textView2 = findViewById(R.id.textView2);
+//
+//        imageView3.setAnimation(topAnim);
+//        textView.setAnimation(bottomAnim);
+//        textView2.setAnimation(bottomAnim);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.splash_activity);
+
+       Animation fadeOut = new AlphaAnimation(1, 0);
+       fadeOut.setInterpolator(new AccelerateInterpolator());
+       fadeOut.setStartOffset(500);
+       fadeOut.setDuration(1800);
+       ImageView image = findViewById(R.id.imageView3);
+
+        image.setAnimation(fadeOut);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+        }, SPLASH_SCREEN_TIMEOUT);
+    }
+}
