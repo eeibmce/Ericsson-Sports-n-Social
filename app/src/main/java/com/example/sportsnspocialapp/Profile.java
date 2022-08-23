6cfa1a88a933e4e418f3ad673d78d48cfaac1696
Alfiec1204 <110679774+Alfiec1204@users.noreply.github.com>
2022-08-23 16:32:48 +0100

    to merge

--- app/src/main/java/com/example/sportsnspocialapp/Profile.java
+++ app/src/main/java/com/example/sportsnspocialapp/Profile.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
